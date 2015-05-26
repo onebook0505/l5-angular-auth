91fbf539d9bcd9d84989343b649bd76e6678ec63
onebook0505 <onebook0505@gmail.com>
2015-05-26 19:33:44 +0800

    使用內建flysystem Upload 圖片至 storage/app

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,6 +10,9 @@ use Illuminate\Http\Response;
 // use Illuminate\Http\Request;
 use Request;
 
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
+
 class UserController extends Controller {
 
 	/**
@@ -34,10 +37,11 @@ class UserController extends Controller {
 	    $destination_path = public_path().'/user-upload/'; // 上傳的檔案要存在伺服器哪個資料夾
 
 	    if (Request::hasFile('user_file')) { // hasFile確認是否有上傳
-	        $upload_success = $file->move($destination_path, $file_name); // 上傳成功就把檔案移至$destination_path、用$file_name重新命名
-	        return "img upload success!";
+	    	Storage::disk('local')->put($file->getFilename().'.'.$extension,  File::get($file));
+	        //$upload_success = $file->move($destination_path, $file_name); // 上傳成功就把檔案移至$destination_path、用$file_name重新命名
+	        return response()->json(array('success' => true));
 	    } else {
-	        return "img upload failed!";
+	        return response()->json(array('success' => false));
 	    }
 
 	    // $user_obj = \Auth::user(); // 現在登入的user存在$user_obj
